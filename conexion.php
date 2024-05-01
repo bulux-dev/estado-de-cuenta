9c99a8a9d5e05252edc3f031a435c25596b2b82e
Buluxd <daniel.bulux@hotmail.com>
2024-04-30 19:37:46 -0600

    on action desde la form para la bd

--- conexion.php
+++ conexion.php
@@ -15,25 +15,9 @@ if ($conn->connect_error) {
 }
 echo "succes";
 
-// Get form data (assuming data is coming from a form)
-$type = $_POST["tipo"];
-$description = $_POST["descripcion"];
-$value = $_POST["valor"];
+$stmt = $conn->prepare("INSERT INTO transactions(type, description, price) VALUES ('egreso','pluma',15)");
 
+$stmt->execute();
 
-// Prepare SQL statement
-$sql = "INSERT INTO Movements (type, description, value)
-VALUES (?, ?, ?)";
-
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $type, $description, $value);
-
-if ($stmt->execute()) {
-  echo "New movement created successfully";
-} else {
-  echo "Error: " . $stmt->error;
-}
-
-$stmt->close();
 $conn->close();
 ?>
