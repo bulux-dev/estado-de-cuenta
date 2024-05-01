e29af89bf9e6ddf90c553d671e474c8ec17a874c
Buluxd <daniel.bulux@hotmail.com>
2024-04-30 19:59:31 -0600

    base de datos funcionando

--- conexion.php
+++ conexion.php
@@ -13,11 +13,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "succes";
 
-$stmt = $conn->prepare("INSERT INTO transactions(type, description, price) VALUES ('egreso','pluma',15)");
+// Access form data
+$type = $_REQUEST["tipo"];
+$description = $_REQUEST["descripcion"];
+$valor = $_REQUEST["valor"];
 
-$stmt->execute();
+// Create SQL statement (replace "your_table_name" with your actual table name)
+$sql = "INSERT INTO transactions (type, description, price) VALUES ('$type', '$description', '$valor')";
+
+if(mysqli_query($conn,$sql)){
+  echo "data inserted succesfully";
+}else{
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
